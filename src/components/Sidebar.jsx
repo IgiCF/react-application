c124d1ef54ef9355603640fbabeeca1f72ac3f81
Isaac Ferreria <is575388@ucf.edu>
2026-02-12 01:52:53 -0500

    Completed React Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,19 +4,28 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
+    if (newMenuItem.trim()=== "")return
+    setMenuItems([newMenuItem, ...menuItems])
+    setNewMenuItem("")
+  }, [newMenuItem, menuItems])
+
+  let filteredMenuItems = menuItems.filter((item) =>
+    item.toLowerCase().includes(filter.toLowerCase())
+  )
+
     console.log("Added menu item")
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
     //   setMenuItems([item, ...menuItems])
-  }, [])
+  
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
@@ -33,10 +42,7 @@ export default function Sidebar() {
       ></input>
       <br />
       <button
-        onClick={() => {
-          /* TODO: 3 */
-        }}
-      >
+        onClick={addMenuItem}>
         Add Item
       </button>
       <br />
@@ -47,6 +53,11 @@ export default function Sidebar() {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+      <ul>
+        {filteredMenuItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
